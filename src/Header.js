6262419a804ba275b516d1cd6cc244344dcf852b
Madhav Bist <bistmadhav5@gmail.com>
2021-01-24 23:09:44 -0600

    Header_Completed

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+    return (
+        <div className = "headerof-app">
+        <h1> My Cart </h1>
+        <h5>Online Sale.com</h5>
+        </div>
+    )
+}
+
+export default Header
